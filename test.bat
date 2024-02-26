4a81cbe538c73dc7e8384d94e6b89c9101e38541
Thad House <thadhouse1@gmail.com>
2024-02-25 22:47:22 -0800

    Thing

---
+++ test.bat
@@ -0,0 +1,7 @@
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+
+ECHO ON
+
+echo %vctoolsversion%
+
+dir "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC"
